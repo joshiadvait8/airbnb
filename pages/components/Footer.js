ca9f5d4a895875695ecec15eb23f3f7facecef8d
joshiadvait8 <joshiadvait8@gmail.com>
2021-08-07 23:17:52 +0530

    day 1 build

---
+++ pages/components/Footer.js
@@ -0,0 +1,46 @@
+function Footer() {
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-4
+        gap-y-10 px-32 py-14 bg-gray-100 text-gray-100">
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">ABOUT</h5>
+                <p>How Airbnb works</p>
+                <p>Newsroom</p>
+                <p>Investors</p>
+                <p>Airbnb plus</p>
+                <p>Airbnb Luxe</p>
+
+            </div>
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">COMMUNITY</h5>
+                <p>Accessibility</p>
+                <p>This is not real site</p>
+                <p>clean clone </p>
+                <p>Referals Accepted</p>
+                <p>Frontend Family</p>
+
+            </div>
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">HOST</h5>
+                <p>Advait Joshi</p>
+                <p>Presents</p>
+                <p>Zero to Full stack Hero</p>
+                <p>Love for frontend</p>
+                <p>Enjoying build</p>
+
+            </div>
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">SUPPORT</h5>
+                <p>Help Center</p>
+                <p>Trust and Safety</p>
+                <p>Say Hi</p>
+                <p>Contact me</p>
+                <p>If you Like it</p>
+
+            </div>
+
+        </div>
+    );
+}
+
+export default Footer
